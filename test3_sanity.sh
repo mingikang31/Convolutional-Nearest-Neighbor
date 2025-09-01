577cc024c2be6437d540838d3b5131451e7f1315
Mingi Kang <mkang2@bowdoin.edu>
2025-09-01 10:11:08 -0400

    DIST + COLOR ConvNN experiments

--- test3_sanity.sh
+++ test3_sanity.sh
@@ -14,29 +14,50 @@ cd /mnt/research/j.farias/mkang2/Convolutional-Nearest-Neighbor
 
 source activate mingi
 
-# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 3 --num_epochs 75 --output_dir ./Output/Aug_30_Sanity/Conv2d_K3
+# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 3 --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/Conv2d_K3
 
-python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 2 --num_epochs 75 --output_dir ./Output/Aug_30_Sanity/Conv2d_K2
+# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 2 --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/Conv2d_K2
 
+# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 1 --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/Conv2d_K1
 
-# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 1 --num_epochs 75 --output_dir ./Output/Aug_30_Sanity/Conv2d_K1
+### DIST + COLOR
+## SEED 0 
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K9_0_coord --padding 1 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K9_0 --padding 1 --seed 0
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K4_0_coord --padding 1 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K4_0 --padding 1 --seed 0
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K1_0_coord --padding 0 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K1_0 --padding 0 --seed 0
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K9_0 --padding 1 --seed 0
 
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K4_0 --padding 1 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K9_42 --padding 1 --seed 42
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K1_0 --padding 0 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K4_42 --padding 1 --seed 42
+## SEED 42
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K9_42_coord --padding 1 --seed 42
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K1_42 --padding 0 --seed 42
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K4_42_coord --padding 1 --seed 42
 
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K1_42_coord --padding 0 --seed 42
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K9_102 --padding 1 --seed 102
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K9_42 --padding 1 --seed 42
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K4_102 --padding 1 --seed 102
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K4_42 --padding 1 --seed 42
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Aug_30_Sanity/ConvNN_All_K1_102 --padding 0 --seed 102
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K1_42 --padding 0 --seed 42
+
+## SEED 102
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K9_102_coord --padding 1 --seed 102
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K4_102_coord --padding 1 --seed 102
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --coordinate_encoding --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K1_102_coord --padding 0 --seed 102
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K9_102 --padding 1 --seed 102
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K4_102 --padding 1 --seed 102
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K1_102 --padding 0 --seed 102
+
---
+++ test4.sh
@@ -0,0 +1,29 @@
+#! /bin/bash 
+#SBATCH --nodes=1 
+#SBATCH --mem=64G
+#SBATCH -p gpu --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=Sanity_Test
+#SBATCH --time=500:00:00
+#SBATCH --output=slurm_out/%j.out
+#SBATCH --error=slurm_out/%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
+#SBATCH --mail-user=mkang2@bowdoin.edu
+
+cd /mnt/research/j.farias/mkang2/Convolutional-Nearest-Neighbor
+
+source activate mingi
+
+# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 3 --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/Conv2d_K3
+
+# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 2 --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/Conv2d_K2
+
+# python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 1 --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/Conv2d_K1
+
+### DIST + COLOR
+## SEED 0 
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K9_0 --padding 1 --seed 0
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K4_0 --padding 1 --seed 0
+
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_1_Sanity/ConvNN_All_K1_0 --padding 0 --seed 0
