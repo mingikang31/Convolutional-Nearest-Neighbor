a085f1a24e25ff552c7e3ae38f67202f0847d0a3
Mingi Kang <mkang2@bowdoin.edu>
2025-09-06 14:36:38 -0400

    Test 4 for Sanity check no bias experiments

--- test4.sh
+++ test4.sh
@@ -15,11 +15,11 @@ cd /mnt/research/j.farias/mkang2/Convolutional-Nearest-Neighbor
 source activate mingi
 
 # ConvNN
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --coordinate_encoding --sampling_type all --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/ConvNN_All_K9_0 --padding 1 --seed 0
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --coordinate_encoding --sampling_type all --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/ConvNN_All_K9_0_p1 --padding 1 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --coordinate_encoding --sampling_type all --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/ConvNN_All_K4_0 --padding 1 --seed 0
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --coordinate_encoding --sampling_type all --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/ConvNN_All_K4_0_p1 --padding 1 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --coordinate_encoding --sampling_type all --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/ConvNN_All_K1_0 --padding 0 --seed 0
+python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 1 --coordinate_encoding --sampling_type all --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/ConvNN_All_K1_0_p0 --padding 0 --seed 0
 
 # Conv2d
 python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 3 --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/Conv2d_K3
@@ -27,8 +27,6 @@ python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --ker
 python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 2 --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/Conv2d_K2
 
 python allconvnet_main.py --layer Conv2d --num_layers 3 --channels 32 16 8 --kernel_size 1 --num_epochs 50 --output_dir ./Output/Sep_6_no_bias/Conv2d_K1
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 9 --coordinate_encoding --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_2_Sanity_Dist/ConvNN_All_K9_0_p0 --padding 0 --seed 0
 
-python allconvnet_main.py --layer ConvNN --num_layers 3 --channels 32 16 8 --K 4 --coordinate_encoding --sampling_type all --num_epochs 75 --output_dir ./Output/Sep_2_Sanity_Dist/ConvNN_All_K4_0 --padding 0 --seed 0
 
 
