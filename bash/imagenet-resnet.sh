a035b66a2996cf81c323a76e8e3655ff96cad4b3
mingikang31 <mkang2@bowdoin.edu>
2026-03-15 01:23:20 -0400

    [HPC] Branching KS = 3 K = 4

--- bash/imagenet-resnet.sh
+++ bash/imagenet-resnet.sh
@@ -18,7 +18,7 @@ cd /mnt/research/j.farias/mkang2/Convolutional-Nearest-Neighbor
 python main.py \
     --model resnet50 \
     --layer Branching \
-    --K 16 \
+    --K 4 \
     --kernel_size 3 \
     --padding 1 \
     --sampling_type all \
@@ -30,4 +30,21 @@ python main.py \
     --pin_memory \
     --batch_size 512 \
     --data_path /mnt/research/j.farias/mkang2/Datasets \
-    --output_dir ./ImageNet-Output/ResNet-50/Branching_All_K16_Ks3/
+    --output_dir ./ImageNet-Output/ResNet-50/Branching_All_K4_Ks3/
+
+python main.py \
+    --model resnet50 \
+    --layer Branching \
+    --K 4 \
+    --kernel_size 3 \
+    --padding 1 \
+    --sampling_type random \
+    --num_samples 32 \
+    --dataset imagenet1k \
+    --compile \
+    --use_amp \
+    --num_workers 12 \
+    --pin_memory \
+    --batch_size 512 \
+    --data_path /mnt/research/j.farias/mkang2/Datasets \
+    --output_dir ./ImageNet-Output/ResNet-50/Branching_Rand_K4_Ks3_N32/
